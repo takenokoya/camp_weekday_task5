e9423ffee217507bd001a35df304cf0e19cbbfc1
takenokoya <takuya1002@gmail.com>
2019-03-09 21:45:44 +0900

    controllerとmoduleのリファクタリング

--- app/controllers/concerns/common.rb
+++ app/controllers/concerns/common.rb
@@ -1,46 +1,37 @@
 module Common
   extend ActiveSupport::Concern
 
-  # Commonモジュールにbirthdayメソッドを定義(リファクタリング)
-  def birthday
-    @birthday = params[:birthday]
-    @year = @birthday[0, 4]  # 8桁の文字列のはじめの4桁を@yearに代入
-    @month = @birthday[4, 2]  # 8桁の文字列の次の2桁を@monthに代入
-    @day = @birthday[6, 2]  # 8桁の文字列の最後の2桁を@dayに代入
-    @monthday = @birthday[4, 4]  # 8桁の文字列の下4桁を@monthdayに代入
-    # 今日が誕生日か判別
-    if @monthday.to_i == Date.today.strftime("%m%d").to_i
-      @your_birthday = "今日があなたの誕生日です。"
+  # 生まれた月と日を渡すと今日が誕生日か判別
+  def confirm_birthday(month, day)
+    monthday = (month + day).to_i
+    if monthday == Date.today.strftime("%m%d").to_i
+      "今日があなたの誕生日です。"
     end
   end
 
-    # Commonモジュールにzodiac_signメソッドを定義
-  def zodiac_sign
-    @zodiac_sign = case @monthday.to_i  # @monthdayに格納された文字列を数値化してcase文でパターンマッチング
-                   when (101..119) then "山羊座"
-                   when (120..218) then "水瓶座"
-                   when (219..320) then "魚座"
-                   when (321..419) then "牡羊座"
-                   when (420..520) then "牡牛座"
-                   when (521..621) then "双子座"
-                   when (622..722) then "蟹座"
-                   when (723..822) then "獅子座"
-                   when (823..922) then "乙女座"
-                   when (923..1023) then "天秤座"
-                   when (1024..1122) then "蠍座"
-                   when (1123..1221) then "射手座"
-                   when (1222..1231) then "山羊座"
-                   end
+  # 生まれた月と日を渡すとcase文でパターンマッチングして星座を返す
+  def determine_zodiac_sign(month, day)
+    monthday = (month + day).to_i
+    case monthday
+    when (101..119) then "山羊座"
+    when (120..218) then "水瓶座"
+    when (219..320) then "魚座"
+    when (321..419) then "牡羊座"
+    when (420..520) then "牡牛座"
+    when (521..621) then "双子座"
+    when (622..722) then "蟹座"
+    when (723..822) then "獅子座"
+    when (823..922) then "乙女座"
+    when (923..1023) then "天秤座"
+    when (1024..1122) then "蠍座"
+    when (1123..1221) then "射手座"
+    when (1222..1231) then "山羊座"
+    end
   end
 
-  # Commonモジュールにageメソッドを定義
-  def age
-    @age = (Date.today.strftime("%Y%m%d").to_i - @birthday.to_i) / 10000
+  # 生年月日を渡すと今日の日付と引き算した結果をもとに年齢を返す
+  def calculate_age(birthday)
+    (Date.today.strftime("%Y%m%d").to_i - birthday.to_i) / 10000
   end
-
-  # Commonモジュールにfortuneメソッドを定義
-  def fortune
-    fortunes = ["大吉", "中吉", "吉", "小吉", "凶", "大凶"]
-    @fortune = fortunes.shuffle![0]
-  end
+  
 end
--- app/controllers/fortunetellings_controller.rb
+++ app/controllers/fortunetellings_controller.rb
@@ -1,11 +1,15 @@
 class FortunetellingsController < ApplicationController
   include Common
-  # showアクションをリファクタリング。ActiveSupport::ConcernのCommonモジュールへ切り出し
+
   def show
-    birthday  # Commonモジュールからメソッドを呼び出す
-    zodiac_sign  # Commonモジュールからメソッドを呼び出す
-    age  # Commonモジュールからメソッドを呼び出す
-    fortune  #Commonモジュールからメソッドを呼び出す
+    @birthday = params[:birthday]
+    @year = @birthday[0, 4]  # 8桁の生年月日のはじめの4桁を@yearに代入
+    @month = @birthday[4, 2]  # 8桁の生年月日の次の2桁を@monthに代入
+    @day = @birthday[6, 2]  # 8桁の生年月日の最後の2桁を@dayに代入
+    @happy_birthday = confirm_birthday(@month, @day)
+    @zodiac_sign = determine_zodiac_sign(@month, @day)
+    @age = calculate_age(@birthday)
+    @fortune = %w(大吉 中吉 吉 小吉 凶 大凶).sample
   end
 
 end
